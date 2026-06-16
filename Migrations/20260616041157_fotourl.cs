533190e9b9e58207a8cf567390ced5f875b6c83a
leonardo falconi <lafr1016@gmail.com>
2026-06-15 23:52:27 -0500

    Arreglos finales de Diseño jugador y album

---
+++ Migrations/20260616041157_fotourl.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace albumMundial.Migrations
+{
+    /// <inheritdoc />
+    public partial class fotourl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FotoUrl",
+                table: "Jugadores",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FotoUrl",
+                table: "Albumes",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FotoUrl",
+                table: "Jugadores");
+
+            migrationBuilder.DropColumn(
+                name: "FotoUrl",
+                table: "Albumes");
+        }
+    }
+}
